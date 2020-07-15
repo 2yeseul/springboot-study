9e22ed445253148e5e2c781688ac18a57c923638
2yeseul <20152917@sungshin.ac.kr>
2020-07-16 00:01:09 +0900

    추가

---
+++ src/main/java/com/studyolle/account/SignUpFormValidator.java
@@ -0,0 +1,30 @@
+package com.studyolle.account;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+@RequiredArgsConstructor // private final 멤버 생성자 만들어줌 (private은 안만들어줌)
+public class SignUpFormValidator implements Validator {
+
+    private AccountRepository accountRepository;
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return aClass.isAssignableFrom(SignUpForm.class);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        // TODO email, nickname
+        SignUpForm signUpForm = (SignUpForm)errors;
+        if(accountRepository.existByEmail(signUpForm.getEmail())) {
+            errors.rejectValue("email", "invalid.email", new Object[]{signUpForm.getEmail()}, "이미 사용중인 이메일 입니다.");
+        }
+        if(accountRepository.existByNickname(signUpForm.getNickname())) {
+            errors.rejectValue("nickname", "invalid.nickname", new Object[]{signUpForm.getNickname()}, "이미 사용중인 닉네임 입니다.");
+        }
+    }
+}
