c08098e5a8ec81de4dc4f96da7d1d1bc24103b07
2yeseul <20152917@sungshin.ac.kr>
2020-07-17 23:45:12 +0900

    mail valid 처리

--- src/main/java/com/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/account/AccountController.java
@@ -1,6 +1,9 @@
 package com.studyolle.account;
 
+import com.studyolle.domain.Account;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -17,6 +20,8 @@ import javax.validation.Valid;
 public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
+    private final AccountRepository accountRepository;
+    private final JavaMailSender javaMailSender;
 
     @InitBinder("signUpForm") // camel 같은 것들 따라감
     public void initBinder(WebDataBinder webDataBinder) {
@@ -36,6 +41,24 @@ public class AccountController {
         if(errors.hasErrors())
             return "account/sign-up";
 
+        // 저장
+        Account account = Account.builder()
+                .email(signUpForm.getEmail())
+                .nickname(signUpForm.getNickname())
+                // password hash로 바꿔서 저장해야함
+                .password(signUpForm.getPassword()) // TODO encoding 해야함
+                .studyCreatedByWeb(true)
+                .studyEnrollmentResultByWeb(true)
+                .studyUpdatedByWeb(true)
+                .build();
+        Account newAccount = accountRepository.save(account);
+        newAccount.generateEmailCheckToken();
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(newAccount.getEmail());
+        mailMessage.setSubject("스터디 올레, 회원 가입 인증"); // 메일 제목
+        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() + "&email="
+                + newAccount.getEmail()); // 링크를 만들어서 보냄
+        javaMailSender.send(mailMessage);
         // TODO 회원 가입 처리
        return "redirect:/";
 
