6ca979c312b4116a5c84bd3df6a6bbdaa1492eac
2yeseul <20152917@sungshin.ac.kr>
2020-07-26 23:56:22 +0900

    회원가입 리팩토링 마무리

--- src/test/java/com/studyolle/account/AccountControllerTest.java
+++ src/test/java/com/studyolle/account/AccountControllerTest.java
@@ -1,13 +1,19 @@
 package com.studyolle.account;
 
+import org.apache.logging.log4j.message.SimpleMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -17,11 +23,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class AccountControllerTest {
 
+    @MockBean
+    JavaMailSender javaMailSender;
+
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
-    private AccountRepository accountRepository;
+    private AccountRepository accountRepository; // user조
 
     @DisplayName("회원 가입 화면 보이는지 테스트")
     @Test
@@ -57,6 +66,7 @@ class AccountControllerTest {
                 .andExpect(view().name("redirect:/"));
 
         assertTrue(accountRepository.existsByEmail("admin@email.com"));
-
+        // SimpleMailMessage를 가지고 아무 클래스의 센드가 호출 되었는지
+        then(javaMailSender).should().send(any(SimpleMailMessage.class));
     }
 }
